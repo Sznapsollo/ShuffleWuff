f03980ae178ed49382c806e0485124201ceddd62
NJ <office@webproject.waw.pl>
2018-10-29 19:44:33 +0100

    navbar for iphone, tweaks to scores

--- inc/js/components/shuffle.js
+++ inc/js/components/shuffle.js
@@ -9,34 +9,51 @@ const Shuffle = {
 					<i v-if="!sharedDictionaryData.loaded" class="fa fa-cog fa-2x fa-spin"></i>
 				</div>
 				<div class="container">
-					Score:<span class="score points" v-bind:class="{ goodScores: score > 0, badScores: score < 0 }">{{score}}</span>
-					Words displayed:<span class="score">{{wordsDisplayed}}</span>
+					Points:<span class="score points" v-bind:class="{ goodScores: points > 0, badScores: points < 0 }">{{points}}</span>
+					Attempts:<span class="score">{{attempts}}</span>
+					Score:<span class="score points">{{score}}%</span>
 				</div>
 				<div class="container">
-					<button type="button" class="btn btn-success btn-lg" v-on:click="changeScore(addPoints)">Good answer</button>
-					<button type="button" class="btn btn-danger btn-lg" v-on:click="changeScore(minusPoints)">Bad answer</button>
+					<button type="button" class="btn btn-success btn-lg" v-on:click="calculateScore(true)">Good answer</button>
+					<button type="button" class="btn btn-danger btn-lg" v-on:click="calculateScore(false)">Bad answer</button>
 				</div>
 				<div class="container">
 					<button type="button" class="btn btn-light" v-on:click="shuffleWord()">Shuffle another</button>
 					<button type="button" class="btn btn-light" v-on:click="cleanScore()">Clean Score</button>
 				</div>
+				<div class="container">
+					<input type="checkbox" name="automatic_shuffle" id="automatic_shuffle_chbx" v-model="automaticShuffle">
+					<label for="checkbox_id">Shuffle automatically after score</label>
+				</div>
 			</div>`,
 		data: function() {
 			return {
 				word : "",
-				wordsDisplayed: 0,
+				attempts: 0,
+				points: 0,
 				score: 0,
-				addPoints: 10,
-				minusPoints: -5,
+				goodPoints: 10,
+				minusPoints: 5,
+				correctAnswers: 0,
+				incorrectAnswers: 0,
+				automaticShuffle: true,
 				sharedDictionaryData
 			}
 		},
 		methods: {
-			changeScore: function(points) {
-				this.score += points;
+			calculateScore: function(isCorrect) {
+				var currObject = this;
 				
-				var whatEffect = points >=0 ? "spinEffect" : "shakeEffect";
-				var whatScores = points >=0 ? "goodScores" : "badScores";
+				if(isCorrect)
+					this.correctAnswers++;
+				else
+					this.incorrectAnswers++;
+				
+				this.points = this.correctAnswers*this.goodPoints - this.incorrectAnswers*this.minusPoints;
+				this.score = this.attempts == 0 ? 0 : ((100*this.correctAnswers)/(this.attempts)).toFixed(0);
+				
+				var whatEffect = isCorrect ? "spinEffect" : "shakeEffect";
+				var whatScores = isCorrect ? "goodScores" : "badScores";
 				
 				$(".result h1").addClass(whatEffect);
 				$(".result h1").addClass(whatScores);
@@ -44,13 +61,16 @@ const Shuffle = {
 				setTimeout(function() {
 					$(".result h1").removeClass(whatEffect);
 					$(".result h1").removeClass(whatScores);
-					}, 1000)
-				
+					
+					if(currObject.automaticShuffle) {
+						currObject.shuffleWord();
+					}
+				}, 1000)
 			},
 			shuffleWord: function () {
 				var currObj = this;
 				this.word = sharedDictionaryData.items[Math.floor(Math.random()*sharedDictionaryData.items.length)];
-				this.wordsDisplayed++;
+				this.attempts++;
 
 				setTimeout(function () {
 					var container = $("#shuffledWord");
@@ -60,8 +80,11 @@ const Shuffle = {
 					});
 			},
 			cleanScore: function() {
+				this.points = 0;
+				this.attempts = 0;
 				this.score = 0;
-				this.wordsDisplayed = 0;
+				this.correctAnswers = 0;
+				this.incorrectAnswers = 0;
 			}
 		},
 		created: function() {
