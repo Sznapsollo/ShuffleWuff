4c16e549fc102baad3dc572f810347ca1fa73962
NJ <office@webproject.waw.pl>
2018-11-03 13:54:59 +0100

    Better dictionary sorting button

--- inc/js/components/dictionary.js
+++ inc/js/components/dictionary.js
@@ -2,10 +2,10 @@ const Dictionary = {
 		template: `
 			<div style="width: 80%; margin: auto auto;">
 				<div style="margin: 10px; display: block; text-align: center" class="form-inline">
-					<button type="button" class="btn btn-light" v-bind:class="{ active: sortOrder==1 }" v-on:click="sortOrder=1">Sort Asc</button>
-					&nbsp;
-					<button type="button" class="btn btn-light" v-bind:class="{ active: sortOrder==2 }" v-on:click="sortOrder=2">Sort Desc</button>
-					&nbsp;
+					<button type="button" class="btn btn-light" v-if="sortOrder == 2" v-on:click="sortOrder=1"><i class="fa fa-sort-down"></i></button>
+					
+					<button type="button" class="btn btn-light" v-if="sortOrder == 1" v-on:click="sortOrder=2"><i class="fa fa-sort-up"></i></button>
+					
 					<div class="btn-group">
 						<input id="searchinput" v-model="searchPhrase" placeholder="search" type="search" class="form-control" />
 						<i v-if="searchPhrase.length > 0" id="searchclear" v-on:click="searchPhrase=''" class="fa fa-times-circle"></i>
